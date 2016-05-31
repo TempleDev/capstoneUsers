52b07abd113ed5a94161099781e3115b65b981d1
Kyler Love <tue94528@temple.edu>
2016-05-31 09:33:18 -0400

    stuff about making an abstract class for web method

---
+++ TempleUser/TempleUser/TempleUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TempleUser
+{
+    public abstract class TempleUser
+    {
+        protected string firstName { get; set; }
+        protected string lastName { get; set; }
+        protected string accessNet { get; set; }
+        protected string email { get; set; }
+    }
+}
